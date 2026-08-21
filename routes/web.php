d070cd1765b060ffa17076bbd0b2d769469471ad
Vilayat Khan <vilayatkhan07@gmail.com>
2026-08-21 13:24:29 +0530

    21 August 2026

--- routes/web.php
+++ routes/web.php
@@ -3,27 +3,52 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\NewsletterController;
+use App\Http\Controllers\PageController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\SearchController;
 use App\Http\Controllers\TagController;
-use App\Http\Controllers\PageController;
-use App\Http\Controllers\ContactController;
 
+/*
+|--------------------------------------------------------------------------
+| Core
+|--------------------------------------------------------------------------
+*/
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/search', [SearchController::class, 'index'])->name('search');
+
+/*
+|--------------------------------------------------------------------------
+| Content
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/post/{slug}', [PostController::class, 'show'])->name('post.show');
 Route::get('/author/{authorProfile:slug}', [AuthorController::class, 'show'])->name('author.show');
 Route::get('/category/{slug}', [CategoryController::class, 'show'])->name('category.show');
 Route::get('/tag/{slug}', [TagController::class, 'show'])->name('tag.show');
-Route::get('/post/{slug}', [PostController::class, 'show'])->name('post.show');
 
-Route::get('/search', [SearchController::class, 'index'])->name('search');
+/*
+|--------------------------------------------------------------------------
+| Newsletter
+|--------------------------------------------------------------------------
+*/
+
 Route::post('/newsletter/subscribe', [NewsletterController::class, 'subscribe'])->name('newsletter.subscribe');
-Route::get('/newsletter/unsubscribe/{token}', [NewsletterController::class, 'unsubscribe'])->name('newsletter.unsubscribe');
+Route::get('/newsletter/unsubscribe/{token}', [NewsletterController::class, 'unsubscribe'])
+    ->middleware('signed')
+    ->name('newsletter.unsubscribe');
+
+/*
+|--------------------------------------------------------------------------
+| Pages
+|--------------------------------------------------------------------------
+*/
 
 Route::get('/about', [PageController::class, 'about'])->name('about');
-
 Route::get('/contact', [ContactController::class, 'show'])->name('contact');
 Route::post('/contact', [ContactController::class, 'submit'])->name('contact.submit');
 
@@ -33,3 +58,13 @@ Route::get('/disclaimer', [PageController::class, 'disclaimer'])->name('disclaim
 Route::get('/cookie-policy', [PageController::class, 'cookies'])->name('cookies');
 Route::get('/affiliate-disclosure', [PageController::class, 'affiliate'])->name('affiliate');
 Route::get('/editorial-policy', [PageController::class, 'editorial'])->name('editorial');
+
+/*
+|--------------------------------------------------------------------------
+| Fallback
+|--------------------------------------------------------------------------
+*/
+
+Route::fallback(function () {
+    abort(404);
+});

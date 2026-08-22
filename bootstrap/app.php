7f5020958ce1e966944377c767435cc020d780eb
Vilayat Khan <vilayatkhan07@gmail.com>
2026-08-23 00:40:13 +0530

    23 August 2026

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,22 +1,20 @@
 <?php
 
+use App\Http\Middleware\HandleRedirects;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use App\Http\Middleware\HandleRedirects;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function ($middleware) {
-        $middleware->append(\App\Http\Middleware\HandleRedirects::class);
-    })
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->append(HandleRedirects::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
